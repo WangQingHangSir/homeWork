cda770d7bce89b088daaa66c14e4e453868b04c5
wangqinghang <1557739168@qq.com>
2022-07-18 21:00:17 +0800

    feat(button): button组件封装 - 块级元素 button组件封装 - loading加载

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,32 @@
 <template>
   <div class="home">
-    <wqhButton>默认按钮</wqhButton>
-    <wqhButton size="medium" type="primary" >成功按钮</wqhButton>
-    <wqhButton type="danger" prefix="tongzhi" suffix="tongzhi">危险按钮</wqhButton>
-    <wqhButton type="warning" border >警告按钮</wqhButton>
+    <wqhButton >默认按钮</wqhButton>
+    <wqhButton disabled :loading="flag" @click="handleSubmit" size="medium" type="primary"  >成功按钮</wqhButton>
+    <wqhButton type="danger" >危险按钮</wqhButton>
+    <wqhButton type="warning" >警告按钮</wqhButton>
   </div>
 </template>
 
 <script>
+
 export default {
-  name: 'HomeView',
-  data() {
+  name: 'Home',
+  data () {
     return {
       flag: false
     }
   },
   components: {
     wqhButton: () => import('../components/button/index.vue')
+  },
+  methods: {
+    handleSubmit () {
+      this.flag = true
+      setTimeout(() => {
+        console.log('123')
+        this.flag = false
+      }, 3000)
+    }
   }
 }
 </script>
