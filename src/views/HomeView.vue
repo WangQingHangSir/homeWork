59481b3b97639d2baa0efadc6ae70b6d04308c38
wangqinghang <1557739168@qq.com>
2022-07-19 20:14:56 +0800

    feat(table组件): table组件封装 - 插槽渲染组件

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home">
-    <wqh-table :column="column" checkbox index></wqh-table>
+    <wqh-table :column="column" checkbox index>
+      <template v-slot:operation>
+        <el-button type="primary">编辑</el-button>
+        <wqh-button type="danger">删除</wqh-button>
+      </template>
+    </wqh-table>
   </div>
 </template>
 
@@ -18,14 +23,22 @@ export default {
             return `<a href="https://www.baidu.com">${data.name}</a>`
           }
         },
+        // { label: '日期', prop: 'date', width: 500 },
         { label: '姓名', prop: 'name' },
         { label: '地址', prop: 'address' },
-        { label: '性别', prop: 'sex' }
+        { label: '性别', prop: 'sex' },
+        {
+          label: '操作',
+          prop: 'operation',
+          type: 'slot',
+          slot_name: 'operation'
+        }
       ]
     }
   },
   components: {
-    wqhTable: () => import('../components/table/index.vue')
+    wqhTable: () => import('../components/table/index.vue'),
+    wqhButton: () => import('../components/button/index.vue')
   },
   methods: {}
 }
