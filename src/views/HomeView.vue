f4e7f165af65d6316d34e5a91c79a7042eccd5f3
wangqinghang <1557739168@qq.com>
2022-07-19 19:55:10 +0800

    feat(table组件): table组件封装 - 定义数据配置表头

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,28 @@
 <template>
   <div class="home">
-    <wqh-table></wqh-table>
+    <wqh-table  :column="column"></wqh-table>
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'Home',
-  data() {
+  data () {
     return {
-      flag: false
+      column: [
+        { label: '日期', prop: 'date' },
+        { label: '姓名', prop: 'name' },
+        { label: '地址', prop: 'address' },
+        { label: '性别', prop: 'sex' }
+      ]
     }
   },
   components: {
     wqhTable: () => import('../components/table/index.vue')
   },
-  methods: {}
+  methods: {
+
+  }
 }
 </script>
